28a5969f3aa82ac869620c8832867030ce399d67
Macho <nonsonnabugwu911@gmail.com>
2026-08-05 13:27:50 +0100

    urls

---
+++ app/urls.py
@@ -0,0 +1,15 @@
+from .views import *
+from django.urls import path
+
+urlpatterns = [
+    path('create-event/', CreateEvent.as_view()),
+    path('create-ticket/', CreateTicket.as_view()),
+    path('view-events/', ViewEvents.as_view()),
+    path('edit-events/', EditEvents.as_view()),
+    path('delete-event/<int:id>/', DeleteEvent.as_view()),
+    path('view-tickets/', ViewTickets.as_view()),
+    path('view-one-ticket/<int:id>/', ViewOneTicket.as_view()),
+    path('get-attendees/<int:id>/', GetAttendees.as_view()),
+    path('check-in/<str:token>/', CheckIn.as_view()),
+    path('payment/', PaymentView.as_view())
+]
